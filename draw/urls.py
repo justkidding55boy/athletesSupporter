b27136406f3304f0f9296274c6f12cd6cb6502ed
eijikudo <eijikudo@berkeley.edu>
2019-07-24 16:14:03 -0400

    second

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^small$', views.small, name='small'),
+    url(r'^large$', views.large, name='large'),
     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
 ]
-
--- draw/views.py
+++ draw/views.py
@@ -4,13 +4,19 @@ import json
 
 def index(request):
     return render(request, 'draw/index.html', {})
+  
+def small(request):
+    return render(request, 'draw/small.html', {})
+  
+def large(request):
+    return render(request, 'draw/large.html', {})
 
 def room(request, room_name):
     room_name;
       
     if room_name == 'room':
       return render(request, 'draw/room.html', {'room_name_json': mark_safe(json.dumps(room_name))
+                                                
 #     elif room_name == 'room2':
-#       return render(request, 'draw/room2.html', {
-#         'room_name_json': mark_safe(json.dumps(room_name))
+#       return render(request, 'draw/room2.html', {'room_name_json': mark_safe(json.dumps(room_name))
     })
